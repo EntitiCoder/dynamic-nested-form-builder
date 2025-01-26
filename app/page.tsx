7347d54258b0422e95d263b073d559132a14f37d
EntitiCoder <nhat.vn@tribox.me>
2025-01-26 18:57:22 +0700

    feat: implement drag and drop to category and fields

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,58 @@
 import FormCheckbox from '@/components/form/checkbox';
 import FormInput from '@/components/form/input';
 import TypeSelect from '@/components/form/type-select';
-import { AddIcon, RemoveIcon } from '@/components/icons';
+import { AddIcon, MoveIcon, RemoveIcon } from '@/components/icons';
 import { FormTypeCreateProfile, schema } from '@/libs/formSchema';
 import { Button } from '@heroui/button';
-import { DevTool } from '@hookform/devtools';
 import { zodResolver } from '@hookform/resolvers/zod';
+import clsx from 'clsx';
+import dynamic from 'next/dynamic';
 import {
   FormProvider,
   useFieldArray,
   useForm,
   useFormContext,
 } from 'react-hook-form';
+
+// Dynamic import for disable loading react beautiful dnd on ssr mode
+
+const DragDropContext = dynamic(
+  () =>
+    import('react-beautiful-dnd').then((mod) => {
+      return mod.DragDropContext;
+    }),
+  { ssr: false }
+);
+
+const Droppable = dynamic(
+  () =>
+    import('react-beautiful-dnd').then((mod) => {
+      return mod.Droppable;
+    }),
+  { ssr: false }
+);
+
+const Draggable = dynamic(
+  () =>
+    import('react-beautiful-dnd').then((mod) => {
+      return mod.Draggable;
+    }),
+  { ssr: false }
+);
+
+// Reused buttons
+
+const RemoveButton = ({ onPress, ...props }) => (
+  <Button color="danger" isIconOnly onPress={onPress} {...props}>
+    <RemoveIcon />
+  </Button>
+);
+
+const MoveButton = ({ ...props }) => (
+  <Button isIconOnly {...props}>
+    <MoveIcon />
+  </Button>
+);
 
 export default function ProfileForm() {
   const form = useForm<FormTypeCreateProfile>({
@@ -32,68 +73,92 @@ export default function ProfileForm() {
     },
     resolver: zodResolver(schema),
   });
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = form;
+  const { handleSubmit } = form;
 
   return (
-    <>
-      <form
-        className="flex flex-col gap-4"
-        onSubmit={handleSubmit((data) => {
-          alert('Form submitted: 🎉');
-          console.log('Form submitted: 🎉', data);
-        })}
-      >
-        <h1 className="text-lg text-center font-bold">Profile Form Builder</h1>
-        <FormProvider {...form}>
-          <CreateNewCategory />
-        </FormProvider>
-        <Button color="primary" type="submit">
-          Submit
-        </Button>
-      </form>
-      <DevTool control={control} placement="top-right" />
-    </>
+    <form
+      className="flex flex-col gap-4"
+      onSubmit={handleSubmit((data) => {
+        alert('Form submitted: 🎉');
+        console.log('Form submitted: 🎉', data);
+      })}
+    >
+      <h1 className="text-lg text-center font-bold">Profile Form Builder</h1>
+      <FormProvider {...form}>
+        <CreateNewCategory />
+      </FormProvider>
+      <Button color="primary" type="submit">
+        Submit
+      </Button>
+    </form>
   );
 }
 
 const CreateNewCategory = () => {
   const methods = useFormContext();
   const { control } = methods;
-  const { append, remove, fields } = useFieldArray({
+  const { append, remove, move, fields } = useFieldArray({
     name: 'category',
     control,
     shouldUnregister: true,
   });
 
+  const handleDragDrop = ({ source, destination }) => {
+    if (destination) {
+      move(source.index, destination.index);
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-4">
-      {fields.map((category, categoryIndex) => (
-        <div
-          key={category.id}
-          className="flex flex-col p-2 border border-gray-200 rounded-lg"
-        >
-          <div className="flex gap-2">
-            <FormInput
-              name={`category.${categoryIndex}.title`}
-              control={control}
-              placeholder="Type your category title ..."
-            />
-            <Button
-              color="danger"
-              isIconOnly
-              onPress={() => remove(categoryIndex)}
-            >
-              <RemoveIcon />
-            </Button>
-          </div>
-          <CreateNewCategoryField categoryIndex={categoryIndex} />
-        </div>
-      ))}
+    <div className="">
+      <DragDropContext onDragEnd={handleDragDrop}>
+        <ul>
+          <Droppable
+            droppableId="category"
+            isDropDisabled={false}
+            isCombineEnabled={false}
+            ignoreContainerClipping={true}
+            direction="vertical"
+          >
+            {(provided, snapshot) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {fields.map((category, categoryIndex) => (
+                  <Draggable
+                    key={category.id}
+                    draggableId={category.id}
+                    index={categoryIndex}
+                  >
+                    {(provided, snapshot) => (
+                      <li
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        className={clsx(
+                          snapshot.isDragging && 'bg-blue-500',
+                          'flex flex-col p-4 border border-gray-200 rounded-lg mb-4'
+                        )}
+                      >
+                        <div className="flex gap-2">
+                          <FormInput
+                            name={`category.${categoryIndex}.title`}
+                            control={control}
+                            placeholder="Type your category title ..."
+                          />
+                          <RemoveButton onPress={() => remove(categoryIndex)} />
+                          <MoveButton {...provided.dragHandleProps} />
+                        </div>
+                        <CreateNewCategoryField categoryIndex={categoryIndex} />
+                      </li>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </ul>
+      </DragDropContext>
       <Button
+        className="mt-2"
         endContent={<AddIcon />}
         onPress={() => append({ fields: [], title: '' })}
       >
@@ -109,64 +174,101 @@ const CreateNewCategoryField = ({
   categoryIndex: number;
 }) => {
   const { control } = useFormContext();
-  const { append, remove, fields } = useFieldArray({
+  const { append, remove, fields, move } = useFieldArray({
     name: `category.${categoryIndex}.fields`,
     control,
     shouldUnregister: true,
   });
 
+  const handleDragDropField = ({ source, destination }) => {
+    if (destination) {
+      move(source.index, destination.index);
+    }
+  };
+
   return (
-    <div className="flex divide-y-2 flex-col gap-4">
-      {fields.map((categoryField, categoryFieldIndex) => (
-        <div
-          key={categoryField.id}
-          className="flex flex-col gap-2  pl-5 pt-3 md:flex-row"
+    <DragDropContext onDragEnd={handleDragDropField}>
+      <div>
+        <ul className="flex divide-y-2 flex-col gap-4">
+          <Droppable
+            droppableId="categoryFields"
+            isDropDisabled={false}
+            isCombineEnabled={false}
+            ignoreContainerClipping={true}
+            direction="vertical"
+          >
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {fields.map((categoryField, categoryFieldIndex) => (
+                  <Draggable
+                    key={categoryField.id}
+                    draggableId={categoryField.id}
+                    index={categoryFieldIndex}
+                  >
+                    {(provided, snapshot) => (
+                      <li
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        className={clsx(
+                          snapshot.isDragging && 'bg-blue-500',
+                          'flex flex-col gap-2  px-5 pt-3  rounded-lg  md:flex-row'
+                        )}
+                      >
+                        <div className="flex gap-2 md:block">
+                          <FormInput
+                            name={`category.${categoryIndex}.fields.${categoryFieldIndex}.title`}
+                            control={control}
+                            placeholder="Type your field title ..."
+                            label="Field"
+                          />
+                          <RemoveButton
+                            className="md:hidden"
+                            onPress={() => remove(categoryFieldIndex)}
+                          />
+                          <MoveButton
+                            className="md:hidden"
+                            {...provided.dragHandleProps}
+                          />
+                        </div>
+                        <div className="block md:flex md:gap-2 md:items-start">
+                          <TypeSelect
+                            name={`category.${categoryIndex}.fields.${categoryFieldIndex}.value`}
+                            control={control}
+                          />
+                          <FormCheckbox
+                            className="mt-2 md:mt-0"
+                            name={`category.${categoryIndex}.fields.${categoryFieldIndex}.isRequired`}
+                            control={control}
+                          />
+                          <RemoveButton
+                            className="hidden md:flex"
+                            onPress={() => remove(categoryFieldIndex)}
+                          />
+                          <MoveButton
+                            className="hidden md:flex"
+                            {...provided.dragHandleProps}
+                          />
+                        </div>
+                      </li>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </ul>
+        <Button
+          className="mt-2 mx-5"
+          radius="lg"
+          variant="shadow"
+          endContent={<AddIcon />}
+          onPress={() => append({ title: '', value: '', isRequired: true })}
         >
-          <div className="flex gap-2 md:block">
-            <FormInput
-              name={`category.${categoryIndex}.fields.${categoryFieldIndex}.title`}
-              control={control}
-              placeholder="Type your field title ..."
-              label="Field"
-            />
-            <Button
-              className="md:hidden"
-              color="danger"
-              isIconOnly
-              onPress={() => remove(categoryFieldIndex)}
-            >
-              <RemoveIcon />
-            </Button>
-          </div>
-          <div className="block md:flex md:gap-2 md:items-start">
-            <TypeSelect
-              name={`category.${categoryIndex}.fields.${categoryFieldIndex}.value`}
-              control={control}
-            />
-            <FormCheckbox
-              className="mt-2 md:mt-0"
-              name={`category.${categoryIndex}.fields.${categoryFieldIndex}.isRequired`}
-              control={control}
-            />
-            <Button
-              className="hidden md:flex"
-              color="danger"
-              isIconOnly
-              onPress={() => remove(categoryFieldIndex)}
-            >
-              <RemoveIcon />
-            </Button>
-          </div>
-        </div>
-      ))}
-      <Button
-        radius="lg"
-        variant="shadow"
-        endContent={<AddIcon />}
-        onPress={() => append({ title: '', value: '', isRequired: true })}
-      >
-        Create a new field
-      </Button>
-    </div>
+          Create a new field
+        </Button>
+      </div>
+    </DragDropContext>
   );
 };
--- components/icons.tsx
+++ components/icons.tsx
@@ -35,3 +35,21 @@ export const AddIcon = (props: IconSvgProps) => (
     />
   </svg>
 );
+
+export const MoveIcon = (props: IconSvgProps) => (
+  <svg
+    className="size-6"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M12 3V21M12 3L9 6M12 3L15 6M12 21L15 18M12 21L9 18M3 12H21M3 12L6 15M3 12L6 9M21 12L18 9M21 12L18 15"
+      stroke="#000000"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
